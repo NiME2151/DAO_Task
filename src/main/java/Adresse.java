36f5f098a7477f277a2a42ecde4ccb70afd87c76
lam <marc.lankau@hec.de>
2023-06-13 11:03:40 +0200

    [ADD] Adresse + Interface

---
+++ src/main/java/Adresse.java
@@ -0,0 +1,64 @@
+public class Adresse implements IAdresse {
+
+    private String strasse;
+    private String hausNr;
+    private String plz;
+    private String ort;
+
+    public Adresse(String strasse, String hausNr, String plz, String ort) {
+        this.strasse = strasse;
+        this.hausNr = hausNr;
+        this.plz = plz;
+        this.ort = ort;
+    }
+
+    @Override
+    public String getStrasse() {
+        return strasse;
+    }
+
+    @Override
+    public String getHausNr() {
+        return hausNr;
+    }
+
+    @Override
+    public String getPlz() {
+        return plz;
+    }
+
+    @Override
+    public String getOrt() {
+        return ort;
+    }
+
+    @Override
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    @Override
+    public void setHausNr(String hausNr) {
+        this.hausNr = hausNr;
+    }
+
+    @Override
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    @Override
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    @Override
+    public String toString() {
+        return "Adresse{" +
+                "strasse='" + strasse + '\'' +
+                ", hausNr='" + hausNr + '\'' +
+                ", plz='" + plz + '\'' +
+                ", ort='" + ort + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/IAdresse.java
@@ -0,0 +1,20 @@
+public interface IAdresse {
+
+    String getStrasse();
+
+    void setStrasse(String strasse);
+
+    String getHausNr();
+
+    void setHausNr(String hausNr);
+
+    String getPlz();
+
+    void setPlz(String plz);
+
+    String getOrt();
+
+    void setOrt(String ort);
+
+    String toString();
+}
