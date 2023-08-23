386ebfd274b73255063aeadb13b0ec5f806cf972
lam <marc.lankau@hec.de>
2023-08-23 11:02:05 +0200

    [WIP] ware

---
+++ src/main/java/kaufvertrag/dataLayer/dataAccessObjects/sqlite/ConnectionManager.java
@@ -0,0 +1,59 @@
+package kaufvertrag.dataLayer.dataAccessObjects.sqlite;
+
+import kaufvertrag.exceptions.DaoException;
+
+import java.sql.*;
+
+public class ConnectionManager {
+
+    private final String CLASSNAME = "ConnectionManager";
+    private static final String CONNECTIONSTRING = "jdbc:sqlite:database.db";
+    private static Connection existingConnection;
+    private static boolean classLoaded = false;
+
+    public static Connection getNewConnection() throws DaoException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(CONNECTIONSTRING);
+            existingConnection = connection;
+            classLoaded = true;
+            return connection;
+        } catch (Exception e) {
+            throw new DaoException(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Connection getExistingConnection() {
+        return existingConnection;
+    }
+
+    public static void close(ResultSet resultSet, Statement statement, Connection connection) throws DaoException {
+        try {
+            resultSet.close();
+        } catch (Exception e) {
+            throw new DaoException(e.getMessage());
+        } finally {
+            try {
+                statement.close();
+            } catch (Exception e) {
+                throw new DaoException(e.getMessage());
+            }
+
+            try {
+                connection.close();
+            } catch (Exception e) {
+                throw new DaoException(e.getMessage());
+            }
+            existingConnection = null;
+            classLoaded = false;
+        }
+    }
+}
