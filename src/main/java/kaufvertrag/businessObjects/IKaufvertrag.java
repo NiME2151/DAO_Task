350af3a771caf7800ca5a63ea3bb316bd0ba43af
lam <marc.lankau@hec.de>
2023-08-30 08:36:01 +0200

    [CHANGE] change interface to Classimplementation

--- src/main/java/kaufvertrag/businessObjects/IKaufvertrag.java
+++ src/main/java/kaufvertrag/businessObjects/IKaufvertrag.java
@@ -6,11 +6,18 @@ import kaufvertrag.dataLayer.businessObjects.Ware;
 public interface IKaufvertrag {
 
     Vertragspartner getVerkaeufer();
+
     void setVerkaeufer(Vertragspartner verkaeufer);
+
     Vertragspartner getKaeufer();
+
     void setKaeufer(Vertragspartner kaeufer);
+
     Ware getWare();
+
     void setWare(Ware ware);
+
     String getZahlungsModalitaeten();
+
     void setZahlungsModalitaeten(String zahlungsModalitaeten);
 }
